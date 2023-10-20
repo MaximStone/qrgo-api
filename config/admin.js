c4d3c7010c22ecfb696cd330cdacfc2f32f9930f
Maxim Kosterin <maxim@oncoshot.com>
2023-10-20 19:15:14 +0200

    commit with package updates

--- config/admin.js
+++ config/admin.js
@@ -2,4 +2,10 @@ module.exports = ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET', 'b4dbad35-6caa-4f84-be56-90d0d3bc2fdf'),
   },
+  apiToken: {
+    salt: "46cc9d71a6455b5809f959d6db3d870d3"
+  },
+  watchIgnoreFiles: [
+    '**/config/sync/**',
+  ],
 });
